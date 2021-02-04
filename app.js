57dfaeefb12b65d909f21b102df42191b487fbc0
saltylassi <illke@naver.com>
2021-02-04 18:17:25 +0900

    feat:Add simple painting function

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const canvas = document.getElementById("jsCanvas");
 const context = canvas.getContext("2d");
 
+canvas.width = 700;
+canvas.height = 700;
+
+context.strokeStyle = "#2c2c2c";
+context.lineWidth = "2.5";
+
 let painting = false;
 
 const onMouseMove = (event) => {
     const { offsetX: x, offsetY: y } = event;
+    if (!painting) {
+        context.beginPath();
+        context.moveTo(x, y);
+    } else {
+        context.lineTo(x, y);
+        context.stroke();
+    }
 };
 
-const onMouseDown = (event) => {
+const startPainting = () => {
     painting = true;
-};
-
-const onMouseUp = (event) => {
-    stopPainting();
 };
 
 const stopPainting = () => {
@@ -21,7 +30,7 @@ const stopPainting = () => {
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
